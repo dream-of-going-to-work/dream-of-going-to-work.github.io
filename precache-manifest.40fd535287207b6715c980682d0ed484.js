04c03cf18ebf0b70a05511b0ea4229b1317c4c22
Lisper <leyapin@gmail.com>
2018-12-10 12:16:10 +0800

    update

---
+++ precache-manifest.40fd535287207b6715c980682d0ed484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02520882cbab0c0eef06",
+    "url": "/static/js/main.02520882.chunk.js"
+  },
+  {
+    "revision": "3528c9989179b81900b5",
+    "url": "/static/js/1.3528c998.chunk.js"
+  },
+  {
+    "revision": "02520882cbab0c0eef06",
+    "url": "/static/css/main.337880eb.chunk.css"
+  },
+  {
+    "revision": "efabda852a57c3d289150ca4885df1db",
+    "url": "/index.html"
+  }
+];
